66e168749f6b2168559927c258240a4688afde38
TIN NGUYEN <130662495+nguyenthanhtin2810@users.noreply.github.com>
2024-05-21 09:11:02 +0700

    Update procedure.sql

--- Database/procedure.sql
+++ Database/procedure.sql
@@ -6,3 +6,13 @@ BEGIN
 END
 GO
 EXEC PROC_CEK_ACCOUNT @userName='phongdaotao' , @passWord='password'
+
+CREATE PROCEDURE PROC_AddPriority
+    @MaDT NVARCHAR(5),
+    @TenDT NVARCHAR(20),
+    @TiLeGiam FLOAT
+AS
+BEGIN
+    INSERT INTO dbo.DTUUTIEN (MaDT, TenDT, TiLeGiam)
+    VALUES (@MaDT, @TenDT, @TiLeGiam);
+END
